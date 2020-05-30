63b6b06f9628e47cf58f6887400c038ebb0f6933
조인영 (In-Young Cho) <ciy405x@kaist.ac.kr>
2020-05-30 20:14:39 +0900

    npy_to_pngs

---
+++ scripts/npys_to_pngs.py
@@ -0,0 +1,36 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+#mpl.rcParams['figure.dpi'] = 250
+
+def ToneMap(c, limit=1.5):
+    # c: (W, H, C=3)
+    luminance = 0.3 * c[:,:,0] + 0.6 * c[:,:,1] + 0.1 * c[:,:,2]
+    col = c[:,:,:]
+    col[:,:,0] /=  (1.0 + luminance / limit)
+    col[:,:,1] /=  (1.0 + luminance / limit)
+    col[:,:,2] /=  (1.0 + luminance / limit)
+    return col
+
+def LinearToSrgb(c):
+    # c: (W, H, C=3)
+    kInvGamma = 1.0 / 2.2
+    return np.clip(c ** kInvGamma, 0.0, 1.0)
+
+for root, dirs, files in os.walk("D:\\p-buffer\\train\\gt", topdown=False):
+    for name in files:
+        npy_fn = os.path.join(root, name)
+        png_fn = os.path.join("D:\\p-buffer\\train\\gt_imgs", name)[:-3]+"png"
+        if os.path.isfile(png_fn):
+            continue
+        
+        img_np = np.load(npy_fn)
+        plt.imsave(os.path.join("D:\\p-buffer\\train\\gt_imgs", name)[:-3]+"png", LinearToSrgb(ToneMap(img_np)))
+
+for root, dirs, files, in os.walk("D:\\p-buffer\\train\\gt_imgs", topdown=False):
+    for name in files:
+        png_fn = os.path.join(root, name)
+        npy_fn = os.path.join("D:\\p-buffer\\train\\gt", name)[:-3]+"npy"
+        if not os.path.isfile(npy_fn):
+            os.remove(png_fn)
