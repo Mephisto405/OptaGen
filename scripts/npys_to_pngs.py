07b8e955a4f6f5ee47889182c61a5942c2f0a191
조인영 (In-Young Cho) <ciy405x@kaist.ac.kr>
2020-05-31 00:15:47 +0900

    remove unnessecary npy files + handshaking test

--- scripts/npys_to_pngs.py
+++ scripts/npys_to_pngs.py
@@ -18,19 +18,31 @@ def LinearToSrgb(c):
     kInvGamma = 1.0 / 2.2
     return np.clip(c ** kInvGamma, 0.0, 1.0)
 
+"""
+cnt = 0
 for root, dirs, files in os.walk("D:\\p-buffer\\train\\gt", topdown=False):
     for name in files:
         npy_fn = os.path.join(root, name)
         png_fn = os.path.join("D:\\p-buffer\\train\\gt_imgs", name)[:-3]+"png"
         if os.path.isfile(png_fn):
             continue
-        
-        img_np = np.load(npy_fn)
-        plt.imsave(os.path.join("D:\\p-buffer\\train\\gt_imgs", name)[:-3]+"png", LinearToSrgb(ToneMap(img_np)))
+        else:
+            os.remove(npy_fn)
+            os.remove(os.path.join("D:\\p-buffer\\train\\input", name)[:-3]+"npy")
+            cnt += 1
+print(cnt)
+"""
 
-for root, dirs, files, in os.walk("D:\\p-buffer\\train\\gt_imgs", topdown=False):
+for root, dirs, files, in os.walk("D:\\p-buffer\\train\\input", topdown=False):
     for name in files:
         png_fn = os.path.join(root, name)
         npy_fn = os.path.join("D:\\p-buffer\\train\\gt", name)[:-3]+"npy"
         if not os.path.isfile(npy_fn):
-            os.remove(png_fn)
+            assert False
+
+for root, dirs, files, in os.walk("D:\\p-buffer\\train\\gt", topdown=False):
+    for name in files:
+        png_fn = os.path.join(root, name)
+        npy_fn = os.path.join("D:\\p-buffer\\train\\input", name)[:-3]+"npy"
+        if not os.path.isfile(npy_fn):
+            assert False
